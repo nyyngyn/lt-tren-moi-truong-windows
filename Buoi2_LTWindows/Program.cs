3b22fc7de5a9692bafdcceef8dc0e15d24dc52d5
Vinh <nyynyy2109@gmail.com>
2026-09-18 16:35:26 +0700

    Add project source files

---
+++ Buoi2_LTWindows/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Student s1 = new Student("Ha Giang");
+        Student s2 = new Student("Ngoc Thao");
+        Student s3 = new Student("Ha Nam");
+
+        s1.Show();
+
+        int num = Student.NumberOfInstance();
+
+        Console.WriteLine("Number of objects: {0}", num);
+
+        Console.ReadLine();
+    }
+}
---
+++ Lab01_02/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab01_02
+{
+    internal class Student
+    {
+        private string Id;
+        private string Name;
+        private int Age;
+
+        // Properties
+        public string Id1 { get => Id; set => Id = value; }
+        public string Name1 { get => Name; set => Name = value; }
+        public int Age1 { get => Age; set => Age = value; }
+
+        // Constructor
+        public Student()
+        {
+            Id = "";
+            Name = "";
+            Age = 0;
+        }
+        public Student(string Id, string Name, int Age)
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        // Method
+        public void Input()
+        {
+            Console.Write("Nhap MSSV: ");
+            Id = Console.ReadLine();
+            Console.Write("Nhap ho va ten SV: ");
+            Name = Console.ReadLine();
+            Console.Write("Nhap tuoi SV: ");
+            Age = int.Parse(Console.ReadLine());
+        }
+        public void show()
+        {
+            Console.WriteLine("MSSV:{0} Ho Ten:{1} Tuoi:{2}", this.Id, this.Name, this.Age);
+        }
+
+    }
+}
